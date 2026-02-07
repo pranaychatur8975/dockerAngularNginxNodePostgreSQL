58a1078ad733fd10ad0f6926e1f27d623057b409
pranaychatur8975 <pranaychatur3@gmail.com>
2026-02-07 16:52:42 +0530

    Update app.js

--- NodeBackend/src/app.js
+++ NodeBackend/src/app.js
@@ -1,5 +1,15 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
+
+// root test route
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
+// API route
+app.get('/api', (req, res) => {
+  res.json({ message: 'API working fine ✅' });
+});
 
 export default app;
